74223ad09471340e283ebd9aeb66a4fb4d290383
Sang Dang <sangdth@gmail.com>
2023-03-08 23:47:26 +0200

    add logo and landing page

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,22 +1,55 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Heading,
+  HStack,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { CustomHead } from '@/components/app/CustomHead';
+import { Logo } from '@/components/Logo';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
 
 export default function Home() {
   return (
-    <div className="flex h-screen bg-black">
-      <Head>
-        <title>Platforms on Vercel</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <CustomHead
+        title="IssueRadar"
+        description="Quickly get biggest issues from a GitHub project"
+      />
 
-      <div className="m-auto w-48">
-        <Image
-          width={512}
-          height={512}
-          src="/logo.png"
-          alt="Platforms on Vercel"
-        />
-      </div>
-    </div>
+      <Container marginY={4} maxWidth="container.md" centerContent>
+        <Stack textAlign="center" spacing={20}>
+          <Center>
+            <Logo width={180} />
+          </Center>
+
+          <Stack spacing={5}>
+            <Heading as="h1" size="4xl" textAlign="center">
+              Keep your project&apos;s issues in sight.
+            </Heading>
+
+            <Text fontSize="2xl">
+              Summarizing Github Repository Issues in One Minute
+            </Text>
+          </Stack>
+
+          <Center>
+            <HStack spacing={5}>
+              <Button colorScheme="green">Request Access</Button>
+              <Button
+                colorScheme="green"
+                variant="outline"
+                rightIcon={<ArrowForwardIcon />}
+              >
+                Login
+              </Button>
+            </HStack>
+          </Center>
+        </Stack>
+      </Container>
+    </>
   );
 }
